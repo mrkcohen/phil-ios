8d77aac413e994312ed947aeaee5376eecec9a2c
Mike Cohen <mrkcohen@gmail.com>
2020-04-01 21:37:47 -0700

    Initial Commit

---
+++ Phil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Phil
+//
+//  Created by Mike Cohen on 4/1/20.
+//  Copyright © 2020 Mike Cohen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
